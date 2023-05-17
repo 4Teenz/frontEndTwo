92ae669f0f0ca52630b0d3f21929697eef80f34e
Du Edu <132024289+4Teenz@users.noreply.github.com>
2023-05-16 21:31:56 -0400

    Add files via upload

--- DuSteam/inicioDaSteam/Script.js
+++ DuSteam/inicioDaSteam/Script.js
@@ -1,40 +1,27 @@
-// const carrosselSlide = document.querySelector(".slide");
-// const carrosselImage = document.querySelectorAll(".slide img");
-// const prevBtn = document.querySelector(".carrosselPrev");
-// const nextBtn = document.querySelector(".carrosselNext");
 let count = 1
 document.getElementById("radio1").checked = true
 
 setInterval(function(){
     nexImage();
 }   ,5000)
+
 function nexImage(){
     count++
     if(count>10){
         count = 1;
     }
-
-    document.getElementById("radio" + count).checked = true
-
+    
+    document.getElementById("radio" + count).checked = true  
+}
+function prevImage(){
+    count--
+    if(count<1){
+        count = 10;
+    }
+    
+    document.getElementById("radio" + count).checked = true  
 }
 
-// function slideTo(index) {
-//     carrosselSlide.style.transform = `translateX(-${index * 800}px)`;
-//   counter = index;
-// }
-
-// prevBtn.addEventListener("click", () => {
-//   if (count === 0) {
-//     slideTo(carrosselImage.length - 1);
-//   } else {
-//     slideTo(count - 1);
-//   }
-// });
-
-// nextBtn.addEventListener("click", () => {
-//   if (count === carrosselImage.length - 1) {
-//     slideTo(0);
-//   } else {
-//     slideTo(count + 1);
-//   }
-// });
+function updateVariavel(evt) {
+    count = evt.target.classList[0]
+}

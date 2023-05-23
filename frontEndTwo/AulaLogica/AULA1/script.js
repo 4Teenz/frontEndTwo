e7326cf021b07592b66033e0d848dbeb49aca6f5
Du Edu <132024289+4Teenz@users.noreply.github.com>
2023-05-22 21:13:25 -0400

    Add files via upload

---
+++ frontEndTwo/AulaLogica/AULA1/script.js
@@ -0,0 +1,97 @@
+// let raio = parseFloat(prompt('Digite o raio:'))
+
+// let area = 3.14*raio**2
+
+// console.log('area')
+
+// alert(area)
+
+
+
+// let base = parseFloat(prompt('Digite a base:'))
+
+// altura = parseFloat(prompt('Digite a altura:'))
+
+// let quadrado = base * altura
+
+// let total = quadrado * 2
+
+// console.log('total')
+
+// alert(total)
+
+
+
+
+// let metro = parseFloat(prompt('Digite o metro que vc quer em centimentros'))
+
+// let total = metro * 100
+
+// console.log ('total')
+
+// alert(total)
+
+
+
+
+// let bimestre1 = parseFloat(prompt('1 Bimestre'))
+
+// bimestre2 = parseFloat(prompt('2 Bimestre'))
+
+// bimestre3 = parseFloat(prompt('3 Bimestre'))
+
+// bimestre4 = parseFloat(prompt('4 Bimestre'))
+
+// let media = bimestre1 + bimestre2 + bimestre3 + bimestre4
+
+// total = media / 4
+
+// console.log = ('total')
+
+// alert(total)
+
+
+
+
+// let valor1 = prompt('Digite o primeiro valor:')
+
+// let valor2 = prompt('Digite o segunddo valor:')
+
+// if (valor1 == valor2){
+//     alert('Os valores são iguais')
+// } else {
+//     alert('Os valores são diferentes')
+// }
+
+
+
+// for( let i = 1; i <= 15; i ++ ) {
+//     console.log( 'Bom dia' + ' - ' + i)
+// }
+
+
+
+// for( let i = 100; i >= 10; i--) {
+//     console.log('i' + ' - ' + i)
+// }
+
+
+
+// for( let i = 100; i <= 200; i++) {
+//     if ( i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// let valor = prompt('Digite um numero > 0')
+
+// for( let i = 0; i<= valor; i++){
+//     console.log(i)
+// }
+
+
+
+
